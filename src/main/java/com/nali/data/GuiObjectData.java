5424b7a2d1fc9b87580a363136f98e36c9e6cc55
SpaceCat <jjcakecat@gmail.com>
2023-12-11 16:08:53 +0700

    put rendering to main

---
+++ src/main/java/com/nali/data/GuiObjectData.java
@@ -0,0 +1,49 @@
+package com.nali.data;
+
+public class GuiObjectData implements BothData
+{
+    public int max_part;
+    public int step_models;
+
+    public GuiObjectData(int step_models, int max_part)
+    {
+        this.step_models = step_models;
+        this.max_part = max_part;
+    }
+
+    @Override
+    public int MaxPart()
+    {
+        return this.max_part;
+    }
+
+    @Override
+    public int StepModels()
+    {
+        return this.step_models;
+    }
+
+    @Override
+    public float Width()
+    {
+        return 1.0F;
+    }
+
+    @Override
+    public float Height()
+    {
+        return 1.0F;
+    }
+
+    @Override
+    public float Scale()
+    {
+        return 1.0F;
+    }
+
+    @Override
+    public int MaxFrame()
+    {
+        return -1;
+    }
+}

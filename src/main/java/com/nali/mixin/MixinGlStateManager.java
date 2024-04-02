d7aa577dbea01d00172633dbc60596a30270bc35
Nali <jjcakecat@gmail.com>
2024-04-02 22:08:22 +0700

    Create MixinGlStateManager.java

---
+++ src/main/java/com/nali/mixin/MixinGlStateManager.java
@@ -0,0 +1,37 @@
+//package com.nali.mixin;
+//
+//import net.minecraft.client.renderer.GlStateManager;
+//import org.lwjgl.opengl.GL11;
+//import org.spongepowered.asm.mixin.Final;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.Overwrite;
+//import org.spongepowered.asm.mixin.Shadow;
+//
+//import java.lang.reflect.Field;
+//
+//@Mixin(GlStateManager.class)
+//public abstract class MixinGlStateManager
+//{
+//    @Shadow
+//    private static int activeTextureUnit;
+//    @Final
+//    @Shadow
+//    private static Object[] textureState = null;
+//
+//    private static Field textureName_textureState_field;
+//
+//    @Overwrite
+//    public static void bindTexture(int texture)
+//    {
+////        if (textureName_textureState_field == null)
+////        {
+////            Field[] field_array = textureState[activeTextureUnit].getClass().getDeclaredFields();
+////            for (Field field : field_array)
+////            {
+////                field;
+////            }
+////        }
+//
+//        GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture);
+//    }
+//}
